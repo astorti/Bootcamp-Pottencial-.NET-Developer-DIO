2c48f15fb16c4f33c6518b4f615d8ebd53fd1330
Andre Storti Andrade <a.storti@hotmail.com>
2022-09-28 21:00:33 -0300

    Implementação desafios intermediários 01, 02 e 03

---
+++ Desafios/DesafioIntermediario01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace DesafioIntermediario01
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+        
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+            for ( int i = 0; i < num.Length; i++ )
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(MajorityElement(num));    
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            int major = nums[0];
+            int count = 1;
+            int newCount = 1;
+            int majority = major;
+            for ( int i = 0; i < nums.Length; i++ )
+            {   
+                count = 1; 
+                major = nums[i];
+                for ( int j = 0; j < nums.Length; j++ )
+                {
+                    if ( major == nums[j] )
+                    {
+                        count++;
+                    }      
+                }
+                
+                if (count > newCount)
+                {
+                    newCount = count;
+                    majority = nums[i];
+                }
+            }
+            return   majority;
+        }
+    }
+}
---
+++ Desafios/DesafioIntermediario03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DesafioIntermediario03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeEntradas = 3;
+            var maiorVelocidade = 0;
+            string[] tartarugas = new string[0];
+      
+            while (quantidadeEntradas > 0) 
+            {
+                var numeroQuantidade = Int32.Parse(Console.ReadLine());
+                
+                if (numeroQuantidade >= 1 && numeroQuantidade <= 500) 
+                {
+                    tartarugas = Console.ReadLine().Split(" ");
+                    maiorVelocidade = Int32.Parse(tartarugas[0]);
+                }
+
+            // TODO: Crie as outras condições necessárias para a resolução do desafio:
+                for ( int i = 1; i < tartarugas.Length; i ++ ) 
+                {
+                    var tartaruga = Int32.Parse(tartarugas[i]);
+                        
+                    if ( maiorVelocidade < tartaruga ) 
+                    {
+                        maiorVelocidade = tartaruga;
+                    }
+                }
+
+                if( maiorVelocidade < 10) 
+                {
+                    Console.WriteLine(1);
+                } 
+                else if ( maiorVelocidade >= 10 &&  maiorVelocidade < 20) 
+                {
+                    Console.WriteLine(2);
+                } 
+                else if ( maiorVelocidade >= 20) 
+                {
+                    Console.WriteLine(3);
+                }
+                else 
+                {
+                    Console.WriteLine("Insira um número entre 1 e 500");
+                }
+
+                quantidadeEntradas--;    
+            }           
+        }
+    }
+}
